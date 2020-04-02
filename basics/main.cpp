4cc8a4894ec40cf04098735f5a5f14e06263d580
developer <developer@localhost.localdomain>
2020-04-02 14:40:18 -0400

    vectors and stuff

--- basics/main.cpp
+++ basics/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <vector>
 using std::cout;
+using std::endl;
+using std::vector;
 
 int main (){
     cout << "Hello!" << "\n";
+
+    //datatypes
+    int x=9;
+    std::string y;
+    y="hello world string!";
+    cout << x << endl;
+    cout << y << endl;
+
+    //vector 1d
+    vector<int> v_1{1,2,3};
+    vector<int> v_2 = {4,5,6};
+    vector<int> v_3;
+    v_3 = {7};
+
+    cout << v_1[0] <<endl;
+    cout << v_1[1] <<endl;
+    cout << v_1[2] <<endl;
+    cout << v_1.size() << "=length" <<endl;
+
+    //vector 2d
+    vector<vector<int>> v4_2d1 {{1,2},{3,4}};
+
+    cout << v4_2d1[0][0] << endl;
+    cout << v4_2d1[0].size() << "=2d length element 0" << endl;
+
+    //auto
+    auto i=9;
+    auto va = {1,2,3};
+
     return 0;
 }
